6754fcdc888f958d59a4330a20219a68556a9cce
Wayne Bishop <bishopwb66@gmail.com>
2016-05-24 14:48:48 +1200

    Fixed cart JS problems and madde Cart page

--- js/faulty-shopping-cart.js
+++ js/faulty-shopping-cart.js
@@ -1,23 +1,23 @@
-// If a shopping cart does not exist in localstorage
+
 
 if( localStorage.getItem('cart') == null ) {
-	//Create the cart with empty array if cart is null 
-	localStorage.setItem('cart', JSON.stringify( [] )); 
+	
+	localStorage.setItem('cart', JSON.stringify( [] ));
 }
-// Extract the cart and convert it back into an Object
-var cart = JSON.parse( localStorage.getItem('cart') );
 
-// Show the contents of the cart
+var cart = JSON.parse( localStorage.getItem( 'cart') );
+
+
 console.log( cart );
 
-// Show the user how many items they have in the cart
 updateCartDisplay();
 
 
-//               Find All the .add-to-cart buttons
+
+
 var addToCartButtons = document.querySelectorAll('.add-to-cart');
 
-// Add click event listeners to them all
+
 for(var i=0; i<addToCartButtons.length; i++) {
 
 	addToCartButtons[i].onclick = addToCart;
@@ -31,7 +31,7 @@ function addToCart()	{
 	var product = {
 		name: productName,
 		price: productPrice
-	} 
+	}
 
 	cart.push(product);
 
@@ -43,7 +43,7 @@ function addToCart()	{
 
 }
 
-// Listen for clicks on the clear cart button
+
 document.querySelector('#clear-cart').onclick = function() {
 
 	localStorage.setItem('cart', JSON.stringify( [] ));
@@ -56,11 +56,11 @@ document.querySelector('#clear-cart').onclick = function() {
 
 function updateCartDisplay() {
 
-	// Get the cart contents
+	// Get cart contents
 	var cart = JSON.parse( localStorage.getItem('cart') );
-	// Count the cart contents and display number on screen
+
 	document.querySelector('#total-cart').innerHTML = cart.length;
-	// Update & show cart table
+
 	showCartTable();
 
 
@@ -70,13 +70,13 @@ function showCartTable() {
 
 	// Find the container that will hold the table
 	var container = document.querySelector('#cart-table');
-	
+
 	// Create the HTML table and style with border via setAttribute
 	var table = document.createElement('table');
-	table.setAttribute('border', '1'); 
+	table.setAttribute('border', '1');
 
 
-	// Create a row to hold the headings
+	// Create row to hold the headings
 	var headingsRow = document.createElement('tr');
 
 	// Create the name heading
@@ -96,14 +96,14 @@ function showCartTable() {
 	// Add the headings row to the table
 	table.appendChild(headingsRow);
 
-	// Grand total
+	// Grandtotal
 	var grandTotal = 0;
 
 	// Loop over all the cart items
 	for (var i=0; i<cart.length; i++) {
 
-		// Get the price of the product and add to the grand total
-		grandTotal += cart[i].price;		
+		// Get product price & add to the GT
+		grandTotal += cart[i].price;
 
 		// Create a row for this product
 		var row = document.createElement('tr');
@@ -111,29 +111,31 @@ function showCartTable() {
 		// Create the product name data element
 		var nameTD = document.createElement('td');
 
-		// Create the product price data element
-		var priceTD = document.createElement('td'); 		
+		// Create product price data element
+		var priceTD = document.createElement('td');
 
 		// Add data to the TD element
-		nameTD.innerHTML = cart[i].name;
-		priceTD.innerHTML = cart[i].price; 
+		nameTD.innerHtml = cart[i].name;
+		priceTD.innerHTML = cart[i].price;
 
 		// Add the TD elements to the row
 		row.appendChild(nameTD);
 		row.appendChild(priceTD);
 
 		// Add this row to the table
-		table .appendChild(row);
+		table.appendChild(row);
 
 	}
 
 	console.log(grandTotal);
+	// console.log(nameTD);
+	// console.log(productPrice);
 
 	var grandTotalRow = document.createElement('tr');
 	var grandTotalTD = document.createElement('td');
 	var fillerTD = document.createElement('td');
 
-	grandTotalTD.innerHTML = 'Grand Total: ' + grandTotal;
+	grandTotal.innerHTML = 'Grand Total' + grandTotal;
 
 	grandTotalRow.appendChild(fillerTD);
 	grandTotalRow.appendChild(grandTotalTD);
@@ -143,10 +145,32 @@ function showCartTable() {
 	// Clear whatever is inside the div
 	container.innerHTML = '';
 
-	// Add the table to the screeen
+	// Add table to the screen
 	container.appendChild(table);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
